fdfdae1033def1e5f3e1fd8b42fb63502db2c994
nathnael zelalem <nathnaelzelalem@gmail.com>
2024-12-14 01:14:06 -0800

    Ready for deployment

--- Back-End/app.js
+++ Back-End/app.js
@@ -5,7 +5,14 @@ const cors = require("cors");
 const carRoute = require("./router/carRoute");
 dotenv.config({ path: "./config.env" });
 const app = express();
-app.use(cors());
+const corsOptions = {
+  origin: process.env.CLIENT_URL || "*",
+  optionsSuccessStatus: 200,
+  methods: ["GET", "POST", "PATCH", "DELETE"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use("/api/car", carRoute);
 
